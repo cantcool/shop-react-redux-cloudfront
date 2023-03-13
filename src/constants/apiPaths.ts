efcf67010ae054ba7d0968c7e8ec1cb3a835daf4
Viachaslau Tsimoshchanka <viachaslau_tsimoshchanka@epam.com>
2023-03-13 14:11:04 +0100

    [TASK 5] Update paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://0ove44vgh5.execute-api.eu-central-1.amazonaws.com/dev",
   order: "https://0ove44vgh5.execute-api.eu-central-1.amazonaws.com/dev",
-  import: "https://0ove44vgh5.execute-api.eu-central-1.amazonaws.com/dev",
+  import: "https://ykyq8ecpvg.execute-api.eu-central-1.amazonaws.com/dev",
   bff: "https://0ove44vgh5.execute-api.eu-central-1.amazonaws.com/dev",
   cart: "https://0ove44vgh5.execute-api.eu-central-1.amazonaws.com/dev",
 };
