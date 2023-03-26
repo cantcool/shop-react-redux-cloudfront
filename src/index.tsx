83296a8861a7661108ffebc17d097531f59d6dff
Viachaslau Tsimoshchanka <viachaslau_tsimoshchanka@epam.com>
2023-03-26 21:50:51 +0200

    [TASK 7] Add axios interceptor for import URL

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import { createRoot } from "react-dom/client";
 import App from "~/components/App/App";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -7,6 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { theme } from "~/theme";
+import API_PATHS from "./constants/apiPaths";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -22,6 +24,17 @@ if (import.meta.env.DEV) {
 const container = document.getElementById("app");
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!);
+
+axios.interceptors.response.use(undefined, (error) => {
+  if (error && error.config.url.indexOf(API_PATHS.import) !== -1) {
+    const { message, name, status } = error;
+    alert(
+      `Import error occured!\nMessage: ${message}\nName: ${name}\nStatus: ${status}`
+    );
+  }
+  return Promise.reject(error.response);
+});
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
